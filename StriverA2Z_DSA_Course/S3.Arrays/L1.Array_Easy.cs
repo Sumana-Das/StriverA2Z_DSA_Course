8d74b9a4645b3406f4839b0070d8823ef0cbe0d0
Sumana Das <dassumana0405@gmail.com>
2025-09-08 11:41:24 +0530

    array problems

--- StriverA2Z_DSA_Course/S3.Arrays/L1.Array_Easy.cs
+++ StriverA2Z_DSA_Course/S3.Arrays/L1.Array_Easy.cs
@@ -78,7 +78,7 @@ namespace StriverA2Z_DSA_Course.S3.Arrays
         /// </summary>
         /// <param name="arr"></param>
         /// <returns></returns>
-        public (int[], int) RemoveDuplicates(int[] arr)
+        public (int[], int) RemoveDuplicates1(int[] arr)
         {
             int start = 0;
             int end = 1;
@@ -96,6 +96,24 @@ namespace StriverA2Z_DSA_Course.S3.Arrays
                 }
             }
             return (arr, start + 1);
+        }
+        /// <summary>
+        /// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public int RemoveDuplicates2(int[] arr)
+        {
+            int idx = 1;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i - 1] != arr[i])
+                {
+                    arr[idx] = arr[i];
+                    idx++;
+                }
+            }
+            return idx;
         }
         /// <summary>
         /// Left rotate the array by one place
@@ -142,6 +160,220 @@ namespace StriverA2Z_DSA_Course.S3.Arrays
                 start++;
                 end--;
             }
+        }
+        /// <summary>
+        /// Find the union from two sorted array
+        /// Brute Force: can use SortedSet<int> and put all the elements from the two arrays
+        /// Optimal: use the power of sorted array by using 2 pointer
+        /// TC: O(mn)
+        /// </summary>
+        /// <param name="nums1"></param>
+        /// <param name="nums2"></param>
+        /// <returns>list of integers</returns>
+        /// [1,3,4,5,9],[1,1,2,2,2,3,4,5,6]
+        public List<int> FindUnion(int[] nums1, int[] nums2)
+        {
+            List<int> union = new();
+            int i = 0, j = 0;
+
+            while(i < nums1.Length && j < nums2.Length)
+            {
+                if (nums1[i] <= nums2[j])
+                {
+                    if(union.Count == 0)
+                    {
+                        union.Add(nums1[i]);
+                    }
+                    else if(union[union.Count - 1] != nums1[i])
+                    {
+                        union.Add(nums1[i]);
+                    }
+                    i++;
+                }
+                else
+                {
+                    if (union.Count == 0)
+                    {
+                        union.Add(nums2[j]);
+                    }
+                    else if (union[union.Count - 1] != nums2[j])
+                    {
+                        union.Add(nums2[j]);
+                    }
+                    j++;
+                }
+            }
+            while(i < nums1.Length)
+            {
+                if (union.Count == 0)
+                {
+                    union.Add(nums1[i]);
+                }
+                else if (union[union.Count - 1] != nums1[i])
+                {
+                    union.Add(nums1[i]);
+                }
+                i++;
+            }
+            while (j < nums2.Length)
+            {
+                if (union.Count == 0)
+                {
+                    union.Add(nums2[j]);
+                }
+                else if (union[union.Count - 1] != nums2[j])
+                {
+                    union.Add(nums2[j]);
+                }
+                j++;
+            }
+            return union;
+        }
+        /// <summary>
+        /// https://leetcode.com/problems/max-consecutive-ones/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int count = 0;
+            int maxCount = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    count++;
+                }
+                else
+                {
+                    maxCount = Math.Max(count, maxCount);
+                    count = 0;
+                }
+            }
+            maxCount = Math.Max(count, maxCount);
+            return maxCount;
+        }
+        /// <summary>
+        /// https://leetcode.com/problems/move-zeroes/
+        /// </summary>
+        /// <param name="nums"></param>
+        public void MoveZeroes(int[] nums)
+        {
+            int idx = 0;
+            if (nums.Length == 1 && nums[0] == 0)
+                return;
+            if (nums.Length == 2 && nums[1] == 0)
+                return;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[idx];
+                    nums[idx] = temp;
+                    if (idx < nums.Length)
+                    {
+                        idx++;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// https://leetcode.com/problems/missing-number/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int MissingNumber(int[] nums)
+        {
+            int missingNum = nums.Length;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                missingNum ^= nums[i] ^ i;
+            }
+            return missingNum;
+        }
+        /// <summary>
+        /// https://leetcode.com/problems/single-number/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int SingleNumber(int[] nums)
+        {
+            int num = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                num ^= nums[i];
+            }
+
+            return num;
+        }
+        /// <summary>
+        /// Find the longest subarray whose sum is k, array can have +ve, -ve and 0s
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int LongestSubarraySum(int[] arr, int k)
+        {
+            Dictionary<int, int> dict = new();
+            int prefixSum = 0;
+            int len = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                prefixSum += arr[i];
+                if (prefixSum == k)
+                {
+                    len = Math.Max(len, i + 1);
+                }
+                if (dict.ContainsKey(prefixSum - k))
+                {
+                    len = Math.Max(len, i - dict[prefixSum - k]);
+                }
+                if (!dict.ContainsKey(prefixSum))
+                {
+                    dict.Add(prefixSum, i);
+                }
+            }
+            return len;
+        }
+        /// <summary>
+        /// Find the longest subarray whose sum is k, array can only have +ve                           
+        /// <param name="arr"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int LongestSubarraySumPositive(int[] arr, int k)
+        {
+            int left = 0, right = 0;
+            int sum = 0;
+            int len = 0;
+
+            while(left <= right)
+            {
+                if (right < arr.Length)
+                    sum += arr[right];
+
+                while(sum > k && left <= right)
+                {
+                    sum -= arr[left];
+                    left++;
+                }
+                if(sum == k)
+                    len = Math.Max(len, right - left + 1);
+
+                if(right < arr.Length - 1)
+                    right++;
+                else
+                    left++;
+            }
+            return len;
         }
     }
 }
---
+++ StriverA2Z_DSA_Course/S3.Arrays/L2.Array_Medium.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StriverA2Z_DSA_Course.S3.Arrays
+{
+    public class Array_Medium
+    {
+        /// <summary>
+        /// Two Sum problem
+        /// Intuition: use of hashing
+        /// Variation: if only true/false should be returned and not index positions, 
+        /// then we can use two pointer, left and right after sorting the array
+        /// https://leetcode.com/problems/two-sum/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!dict.ContainsKey(target - nums[i]))
+                {
+                    dict.TryAdd(nums[i], i);
+                }
+                else
+                {
+                    return new int[] { dict[target - nums[i]], i };
+                }
+            }
+            return new int[] { };
+        }
+        /// <summary>
+        /// https://leetcode.com/problems/search-a-2d-matrix/
+        /// Intuition: flatten the array in mind and try binary search
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            int n = matrix.Length;
+            int m = matrix[0].Length;
+            int left = 0, right = n * m - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                int row = mid / m; // row is multiple of col length
+                int col = mid % m; // col is remainder of the division with col length
+
+                if (matrix[row][col] == target)
+                {
+                    return true;
+                }
+                else if (matrix[row][col] < target)
+                {
+                    left = mid + 1;
+                }
+                else if (matrix[row][col] > target)
+                {
+                    right = mid - 1;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// Find the elements whose right side items are greater than that
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public List<int> LeadersInArray(int[] arr)
+        {
+            List<int> listA = new();
+
+            int max = Int32.MinValue;
+
+            for(int i = 0; i <= 0; i++)
+            {
+                if (arr[i] > max)
+                {
+                    listA.Add(arr[i]);
+                }
+                max = Math.Max(max, arr[i]);
+            }
+            return listA;
+        }
+        /// <summary>
+        /// https://leetcode.com/problems/spiral-matrix/description/
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public IList<int> SpiralMatrix(int[][] matrix)
+        {
+            IList<int> spiral = new List<int>();
+            int left = 0;
+            int right = matrix[0].Length - 1;
+            int top = 0;
+            int bottom = matrix.Length - 1;
+
+            while (left <= right && top <= bottom)
+            {
+                //Traverse the top row from left to right
+                //moving right across the top row
+                for (int i = left; i <= right; i++)
+                {
+                    spiral.Add(matrix[top][i]);
+                }
+                //then move the top boundary down means increase it by 1
+                top++;
+                //Traverse the right column from top to bottom
+                //down the rightmost column
+                for (int i = top; i <= bottom; i++)
+                {
+                    spiral.Add(matrix[i][right]);
+                }
+                //then move the right boundary left means decrease it by 1
+                right--;
+                //Check if top <= bottom
+                if (top <= bottom)
+                {
+                    //traverse the bottom row from right to left
+                    //left across the bottom row
+                    for (int i = right; i >= left; i--)
+                    {
+                        spiral.Add(matrix[bottom][i]);
+                    }
+                    //then move the bottom boundary up means decrease it by 1
+                    bottom--;
+                }
+                //Check if left <= right
+                if (left <= right)
+                {
+                    //traverse the left column from bottom to top
+                    //up the leftmost column
+                    for (int i = bottom; i >= top; i--)
+                    {
+                        spiral.Add(matrix[i][left]);
+                    }
+                    //then move the left boundary right
+                    left++;
+                }
+            }
+            return spiral;
+        }
+        /// <summary>
+        /// https://leetcode.com/problems/rotate-image/
+        /// Brute force: take another 2d array and then put all the elements in correct space,
+        /// which is ans[j][n - 1 - i] = mat[i][j]
+        /// Optimal Intuition: either side of the diagonal is opposite, so transpose and then reverse
+        /// </summary>
+        /// <param name="matrix"></param>
+        public void Rotate(int[][] matrix)
+        {
+            Transpose(matrix);
+            Reverse(matrix);
+        }
+        private void Transpose(int[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length - 1; i++)
+            {
+                for (int j = i + 1; j < matrix[0].Length; j++)
+                {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+        private void Reverse(int[][] matrix)
+        {
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                int start = 0;
+                int end = matrix[0].Length - 1;
+
+                while (start < end)
+                {
+                    int temp = matrix[i][start];
+                    matrix[i][start] = matrix[i][end];
+                    matrix[i][end] = temp;
+
+                    start++;
+                    end--;
+                }
+            }
+        }
+    }
+}
